90a8e29a2e2d667312bdcd92b2aec6f10cacee86
arloor <admin@arloor.com>
2024-04-24 14:19:32 +0800

    Update handle_event function in trace_conn library

--- trace_conn/examples/example2.rs
+++ trace_conn/examples/example2.rs
@@ -1,6 +1,19 @@
+use std::net::Ipv4Addr;
+use trace_conn::Event;
+fn handle_event(_cpu: i32, data: &[u8]) {
+    let mut event = Event::default();
+    plain::copy_from_bytes(&mut event, data).expect("Event data buffer was too short");
+
+    match event.tag {
+        0 => println!("ip event: {}", Ipv4Addr::from(event.ip)),
+        1 => println!("host event: {}", String::from_utf8_lossy(&event.hostname)),
+        _ => {}
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     // find / -name libc.so.6 
     let glibc = "/usr/lib64/libc.so.6";
     // let glibc = "/lib64/libc.so.6";
-    trace_conn::start(glibc)
+    trace_conn::start(glibc,handle_event)
 }
--- trace_conn/src/lib.rs
+++ trace_conn/src/lib.rs
@@ -1,14 +1,13 @@
 #![deny(warnings)]
+use libbpf_rs::skel::{OpenSkel, SkelBuilder};
 use libbpf_rs::PerfBufferBuilder;
+use object::{Object, ObjectSymbol};
 use plain::Plain;
 use std::fs;
-use std::net::Ipv4Addr;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
-use libbpf_rs::skel::{OpenSkel, SkelBuilder};
-use object::{Object, ObjectSymbol};
 
 mod prog {
     include!(concat!(
@@ -18,12 +17,12 @@ mod prog {
 }
 use prog::*;
 
-type DynError=Box<dyn std::error::Error>;
-
-type Event = tracecon_types::event;
+type DynError = Box<dyn std::error::Error>;
+// export Event
+pub type Event = tracecon_types::event;
 unsafe impl Plain for Event {}
 
-fn bump_memlock_rlimit() -> Result<(),DynError> {
+fn bump_memlock_rlimit() -> Result<(), DynError> {
     let rlimit = libc::rlimit {
         rlim_cur: 128 << 20,
         rlim_max: 128 << 20,
@@ -36,7 +35,7 @@ fn bump_memlock_rlimit() -> Result<(),DynError> {
     Ok(())
 }
 
-fn get_symbol_address(so_path: &str, fn_name: &str) -> Result<usize,DynError> {
+fn get_symbol_address(so_path: &str, fn_name: &str) -> Result<usize, DynError> {
     let path = Path::new(so_path);
     let buffer = fs::read(path)?;
     let file = object::File::parse(buffer.as_slice())?;
@@ -54,18 +53,10 @@ fn get_symbol_address(so_path: &str, fn_name: &str) -> Result<usize,DynError> {
     Ok(symbol.address() as usize)
 }
 
-fn handle_event(_cpu: i32, data: &[u8]) {
-    let mut event = Event::default();
-    plain::copy_from_bytes(&mut event, data).expect("Event data buffer was too short");
-
-    match event.tag {
-        0 => println!("ip event: {}", Ipv4Addr::from(event.ip)),
-        1 => println!("host event: {}", String::from_utf8_lossy(&event.hostname)),
-        _ => {}
-    }
-}
-
-pub fn start(glibc:&str) -> Result<(),DynError> {
+pub fn start<F>(glibc: &str, handler: F) -> Result<(), DynError>
+where
+    F: FnMut(i32, &[u8]),
+{
     println!("start trace connection");
     let mut skel_builder = TraceconSkelBuilder::default();
 
@@ -79,15 +70,15 @@ pub fn start(glibc:&str) -> Result<(),DynError> {
     let mut skel = open_skel.load()?;
     let address = get_symbol_address(glibc, "getaddrinfo")?;
 
-    let _uprobe =
-        skel.progs_mut()
-            .getaddrinfo_enter()
-            .attach_uprobe(false, -1, glibc, address)?;
+    let _uprobe = skel
+        .progs_mut()
+        .getaddrinfo_enter()
+        .attach_uprobe(false, -1, glibc, address)?;
 
-    let _uretprobe =
-        skel.progs_mut()
-            .getaddrinfo_exit()
-            .attach_uprobe(true, -1, glibc, address)?;
+    let _uretprobe = skel
+        .progs_mut()
+        .getaddrinfo_exit()
+        .attach_uprobe(true, -1, glibc, address)?;
 
     let _kprobe = skel
         .progs_mut()
@@ -100,7 +91,7 @@ pub fn start(glibc:&str) -> Result<(),DynError> {
         .attach_kprobe(true, "tcp_v4_connect")?;
 
     let perf = PerfBufferBuilder::new(skel.maps_mut().events())
-        .sample_cb(handle_event)
+        .sample_cb(handler)
         .build()?;
 
     let running = Arc::new(AtomicBool::new(true));
