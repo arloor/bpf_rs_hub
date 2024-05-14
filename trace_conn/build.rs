75314e34d9a4657c07b496af0db3207351358a02
arloor <admin@arloor.com>
2024-05-14 17:58:04 +0800

    chore: Update libbpf-rs dependency to version 0.23 with vendored feature

--- trace_conn/build.rs
+++ trace_conn/build.rs
@@ -15,16 +15,16 @@ fn main() {
     let mut builder = SkeletonBuilder::new();
     let builder = builder.source(SRC);
     // 使用本地的vmlinux.h
-    // builder.clang_args(["-I."]);
-    {
-        use std::ffi::OsStr;
-        let arch = env::var("CARGO_CFG_TARGET_ARCH")
-        .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
-        builder.clang_args([
-            OsStr::new("-I"),
-            vmlinux::include_path_root().join(arch).as_os_str(),
-        ]);
-    }
+    builder.clang_args(["-I."]);
+    // {
+    //     use std::ffi::OsStr;
+    //     let arch = env::var("CARGO_CFG_TARGET_ARCH")
+    //     .expect("CARGO_CFG_TARGET_ARCH must be set in build script");
+    //     builder.clang_args([
+    //         OsStr::new("-I"),
+    //         vmlinux::include_path_root().join(arch).as_os_str(),
+    //     ]);
+    // }
     builder.build_and_generate(&out).unwrap();
     println!("cargo:rerun-if-changed={SRC}");
 }
