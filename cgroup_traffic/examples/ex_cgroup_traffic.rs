64cb4c54c6fdf53b1a73e8a8043977b7f9ad53c2
arloor <admin@arloor.com>
2024-09-26 14:32:18 +0800

    feat: update to libbpf-rs 0.24

--- cgroup_traffic/examples/ex_cgroup_traffic.rs
+++ cgroup_traffic/examples/ex_cgroup_traffic.rs
@@ -1,12 +1,24 @@
 #![deny(warnings)]
-use std::{mem::MaybeUninit, os::fd::AsRawFd, thread::sleep, time::Duration};
+use std::{error::Error, mem::MaybeUninit, os::fd::AsRawFd, thread::sleep, time::Duration};
 
 use cgroup_traffic::get_self_cgroup;
 
 type DynError = Box<dyn std::error::Error>;
 
 pub fn main() -> Result<(), DynError> {
-    let open_object = Box::leak(Box::new(MaybeUninit::uninit())); // make it live forever, so that this program won't drop
+    let cgroup_transmit_counter = init_cgroup_traffic_monitor()?;
+    loop {
+        println!(
+            "current bytes: {} {}",
+            cgroup_transmit_counter.get_egress(),
+            cgroup_transmit_counter.get_ingress()
+        );
+        sleep(Duration::from_secs(1));
+    }
+}
+
+fn init_cgroup_traffic_monitor() -> Result<cgroup_traffic::CgroupTransmitCounter, Box<dyn Error>> {
+    let open_object = Box::leak(Box::new(MaybeUninit::uninit()));
     let mut cgroup_transmit_counter = cgroup_traffic::attach_self_cgroup(open_object)?;
     let cgroup = get_self_cgroup()?;
     println!(
@@ -16,19 +28,12 @@ pub fn main() -> Result<(), DynError> {
     let f = std::fs::OpenOptions::new()
         .read(true)
         .write(false)
-        .open(cgroup.0)?; // standalone line, to make file live longer
+        .open(cgroup.0)?;
     let cgroup_fd = f.as_raw_fd();
     let progs = &mut cgroup_transmit_counter.skel.progs;
     let link_egress = progs.count_egress_packets.attach_cgroup(cgroup_fd)?;
-    Box::leak(Box::new(link_egress)); // make link live forever, so that this program won't dettach cgroup
+    Box::leak(Box::new(link_egress));
     let link_ingress = progs.count_ingress_packets.attach_cgroup(cgroup_fd)?;
-    Box::leak(Box::new(link_ingress)); // make link live forever, so that this program won't dettach cgroup
-    loop {
-        println!(
-            "current bytes: {} {}",
-            cgroup_transmit_counter.get_egress(),
-            cgroup_transmit_counter.get_ingress()
-        );
-        sleep(Duration::from_secs(1));
-    }
+    Box::leak(Box::new(link_ingress));
+    Ok(cgroup_transmit_counter)
 }
