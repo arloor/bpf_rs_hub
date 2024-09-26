5a8bc7ca7386b7a4d0270131cdab528cfd7f93a1
arloor <admin@arloor.com>
2024-09-27 01:26:36 +0800

    refactor: refactor cgroup_traffic module and update libbpf-rs to 0.24, and add env_logger as dev-dependency

--- cgroup_traffic/examples/ex_cgroup_traffic.rs
+++ cgroup_traffic/examples/ex_cgroup_traffic.rs
@@ -1,12 +1,17 @@
 #![deny(warnings)]
 use std::{thread::sleep, time::Duration};
 
+use log::info;
+
 type DynError = Box<dyn std::error::Error>;
 
 pub fn main() -> Result<(), DynError> {
+    let _ = env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init();
     let cgroup_transmit_counter = cgroup_traffic::init_self_cgroup_skb_monitor()?;
     loop {
-        println!(
+        info!(
             "current bytes: {} {}",
             cgroup_transmit_counter.get_egress(),
             cgroup_transmit_counter.get_ingress()
