8b9086d2de32e778a184fc08d5e0ced03d1a9913
arloor <admin@arloor.com>
2024-09-28 01:50:49 +0800

    Refactor cgroup_traffic module and update cgroup_traffic::init_cgroup_skb_for_process_name function

--- cgroup_traffic/examples/ex_cgroup_traffic.rs
+++ cgroup_traffic/examples/ex_cgroup_traffic.rs
@@ -5,7 +5,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .try_init();
-    let cgroup_transmit_counter = cgroup_traffic::init_cgroup_skb_for_process_name("sshd|nginx")?;
+    let cgroup_transmit_counter = cgroup_traffic::init_cgroup_skb_for_process_name("sshd|nginx|rust-analyzer")?;
     loop {
         info!(
             "current bytes: {} {}",
